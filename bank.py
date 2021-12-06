07b373a2b5c37ac0b5e9839203a76b1bcda0bde4
Sat Garcia <sat@sandiego.edu>
2021-12-06 10:07:14 -0800

    init files

---
+++ bank.py
@@ -0,0 +1,16 @@
+"""
+Module to help practice for Quiz 10 / Final Exam
+"""
+
+class BankCustomer:
+    def __init__(self, cust_id, account_id, initial_balance):
+        self.customer_id = cust_id
+        # Q1: Initialize accounts instance variable to contain a single account
+        #   with the given ID and balance
+
+    # Q2: add your add_account method here
+
+    # Q3: add your get_total_balance method here
+
+
+# Add your tester code (from Q4) here
